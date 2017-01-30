acd17ab147b5dd04f754e928a581d4c37b42fc3f
Stephen Fox <stephenfox995@hotmail.com>
2017-01-30 14:01:30 +0000

    Updated code

--- Lab1/index.js
+++ Lab1/index.js
@@ -1,50 +1,49 @@
-var express = require('express'),
-    massive = require('massive');
+const express = require('express');
+const massive = require('massive');
 
-let app = express();
-let db = massive.connectSync({db: 'pgguide'});
+const app = express();
+const db = massive.connectSync({ db: 'pgguide' });
 
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Listening on port 3000!');
 });
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', (req, res) => {
+  res.send('Welcome!');
 });
 
-app.get('/users', function (req, res) {
-  db.run('select * from users', function (err, results) {
-    console.log(results);
+app.get('/users', (req, res) => {
+  db.run('select * from users', (err, results) => {
     res.send(results);
   });
 });
-app.get('/users/:id', function (req, res) {
-  let id = req.params.id;
-  db.users.find({"id": id}, function(err, result) {
-    res.send(result);
-  })
-});
-
-app.get('/products', function (req, res) {
-  db.run('select * from products', function (err, result) {
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id;
+  db.users.find({ id: id }, (err, result) => {
     res.send(result);
   });
 });
-app.get('/products/:id', function (req, res) {
-  let id = req.params.id;
-  db.products.find({"id": id}, function(err, result) {
-    res.send(result);
-  })
-});
 
-app.get('/purchases', function (req, res) {
-  db.run('select * from purchases', function (err, result) {
+app.get('/products', (req, res) => {
+  db.run('select * from products', (err, result) => {
     res.send(result);
   });
 });
-app.get('/purchases/:id', function (req, res) {
-  let id = req.params.id;
-  db.purchases.find({"id": id}, function(err, result) {
+app.get('/products/:id', (req, res) => {
+  const id = req.params.id;
+  db.products.find({ id: id }, (err, result) => {
     res.send(result);
-  })
+  });
+});
+
+app.get('/purchases', (req, res) => {
+  db.run('select * from purchases', (err, result) => {
+    res.send(result);
+  });
+});
+app.get('/purchases/:id', (req, res) => {
+  const id = req.params.id;
+  db.purchases.find({ "id": id }, (err, result) => {
+    res.send(result);
+  });
 });
