80a3468ce45c0992fc86a3ba70efe87277c84c0a
Stephen Fox <stephenfox995@hotmail.com>
2017-02-06 09:57:33 +0000

    Updated codestyle

--- Lab1/index.js
+++ Lab1/index.js
@@ -4,46 +4,32 @@ const massive = require('massive');
 const app = express();
 const db = massive.connectSync({ db: 'pgguide' });
 
+app.listen(3000, () => console.log('Listening on port 3000!'));
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!');
-});
 app.get('/', (req, res) => {
   res.send('Welcome!');
 });
 
 app.get('/users', (req, res) => {
-  db.run('select * from users', (err, results) => {
-    res.send(results);
-  });
+  db.run('select * from users', (err, result) => res.send(result));
 });
 app.get('/users/:id', (req, res) => {
   const id = req.params.id;
-  db.users.find({ id: id }, (err, result) => {
-    res.send(result);
-  });
+  db.users.find({ id }, (err, result) => res.send(result));
 });
 
 app.get('/products', (req, res) => {
-  db.run('select * from products', (err, result) => {
-    res.send(result);
-  });
+  db.run('select * from products', (err, result) => res.send(result));
 });
 app.get('/products/:id', (req, res) => {
   const id = req.params.id;
-  db.products.find({ id: id }, (err, result) => {
-    res.send(result);
-  });
+  db.products.find({ id }, (err, result) => res.send(result));
 });
 
 app.get('/purchases', (req, res) => {
-  db.run('select * from purchases', (err, result) => {
-    res.send(result);
-  });
+  db.run('select * from purchases', (err, result) => res.send(result));
 });
 app.get('/purchases/:id', (req, res) => {
   const id = req.params.id;
-  db.purchases.find({ "id": id }, (err, result) => {
-    res.send(result);
-  });
+  db.purchases.find({ id }, (err, result) => res.send(result));
 });
