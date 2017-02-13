d51fdd7893c0d20dfa75ac7064e7acfaa1d9fe23
Stephen Fox <stephenfox995@hotmail.com>
2017-02-13 10:02:35 +0000

    Added Lab2

---
+++ Lab2/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const massive = require('massive');
+
+const app = express();
+const db = massive.connectSync({ db: 'pgguide' });
+app.listen(3000, () => console.log('Listening on port 3000!'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome!');
+});
+
+//  Users endpoints
+app.get('/users', (req, res) => {
+  db.run('select * from users', (err, result) => res.send(result));
+});
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id;
+  db.users.find({ id }, (err, result) => res.send(result));
+});
+
+// Products endpoints
+app.get('/products', (req, res) => {
+  const title = req.query.title;
+  db.run(`select * from products where title = ${title};`, (err, result) => res.send(result));
+});
+app.get('/products/:id', (req, res) => {
+  const id = req.params.id;
+  db.products.find({ id }, (err, result) => res.send(result));
+});
+
+// Purchases endpoints
+app.get('/purchases', (req, res) => {
+  db.run('select * from purchases', (err, result) => res.send(result));
+});
+app.get('/purchases/:id', (req, res) => {
+  const id = req.params.id;
+  db.purchases.find({ id }, (err, result) => res.send(result));
+});
