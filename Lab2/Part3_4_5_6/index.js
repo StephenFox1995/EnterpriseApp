f07d1fa534ee7cf4adfb94eafa50d3f41fe2e62c
Stephen Fox <stephenfox995@hotmail.com>
2017-02-14 01:18:03 +0000

    Added endpoints

--- Lab2/Part3_4_5_6/index.js
+++ Lab2/Part3_4_5_6/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const sequelize = require('./connection')('postgres://@localhost:5432/test');
+require('./connection')('postgres://@localhost:5432/test');
 const models = require('./models')();
 const Validator = require('./validator');
-
 
 
 const app = express();
@@ -12,15 +11,54 @@ app.use(bodyParser.json());
 app.listen(3000, () => console.log('Listening on port 3000!'));
 
 
-
-// returns all participents
-app.get('/participent/:id', (req, res) => {
-  const id = req.params.id;
-  models.Participent.findAll({ where: { id } }).then(data => res.send(data));
+// C
+app.post('/judge', validator.validateJudge, (req, res) => {
+  const judge = req.judge;
+  models.Judge.create(judge)
+    .then(() => res.send('success'))
+    .catch(() => res.send('Failed'));
 });
+// R
+app.get('/judge/:id', (req, res) => {
+  const id = req.params.id;
+  models.Judge.findAll({ where: { id } }).then(data => res.send(data));
+});
+
+// C
+app.post('/courtroom', validator.validateCourtroom, (req, res) => {
+  const courtroom = req.courtroom;
+  models.Courtroom.create(courtroom)
+    .then(() => res.send('success'))
+    .catch(() => res.send('Failed'));
+});
+// R
+app.get('/courtroom/:id', (req, res) => {
+  const id = req.params.id;
+  models.Courtroom.findAll({ where: { id } }).then(data => res.send(data));
+});
+
+// C
 app.post('/participent', validator.validateParticipent, (req, res) => {
   const participent = req.participent;
   models.Participent.create(participent)
     .then(() => res.send('success'))
     .catch(() => res.send('Failed'));
+});
+// R
+app.get('/participent/:id', (req, res) => {
+  const id = req.params.id;
+  models.Participent.findAll({ where: { id } }).then(data => res.send(data));
+});
+
+// C
+app.post('/case', validator.validateCase, (req, res) => {
+  const _case = req.case;
+  models.Participent.create(_case)
+    .then(() => res.send('success'))
+    .catch(() => res.send('Failed'));
+});
+// R
+app.get('/case/:id', (req, res) => {
+  const id = req.params.id;
+  models.Participent.findAll({ where: { id } }).then(data => res.send(data));
 });
--- Lab2/Part3_4_5_6/validator.js
+++ Lab2/Part3_4_5_6/validator.js
@@ -8,16 +8,53 @@ Validator.prototype.validateParticipent = (req, res, next) => {
     return res.status(422).json(failedMessage('Failed', 'No participent object'));
   }
   if (!('name' in participent)) {
-    return res.status(422).json(failedMessage('Failed', 'No participent object'));
+    return res.status(422).json(failedMessage('Failed', 'No name given'));
   }
   if (!('address' in participent)) {
-    return res.status(422).json(failedMessage('Failed', 'No address object'));
+    return res.status(422).json(failedMessage('Failed', 'No address given'));
   }
   if (!('type' in participent)) {
-    return res.status(422).json(failedMessage('Failed', 'No type object'));
+    return res.status(422).json(failedMessage('Failed', 'No type given'));
   }
   req.participent = participent;
   return next();
+};
+
+Validator.prototype.validateCourtroom = (req, res, next) => {
+  const courtroom = req.body.courtroom || null;
+  if (!courtroom) {
+    return res.status(422).json(failedMessage('Failed', 'No courtroom object'));
+  }
+  if (!('number' in courtroom)) {
+    return res.status(422).json(failedMessage('Failed', 'No number given'));
+  }
+  req.courtroom = courtroom;
+  return next();
+};
+
+Validator.prototype.validateJudge = (req, res, next) => {
+  const judge = req.body.judge || null;
+  if (!judge) {
+    return res.status(422).json(failedMessage('Failed', 'No judge object'));
+  }
+  if (!('name' in judge)) {
+    return res.status(422).json(failedMessage('Failed', 'No name given'));
+  }
+  if (!('room' in judge)) {
+    return res.status(422).json(failedMessage('Failed', 'No room given'));
+  }
+  if (!('ext' in judge)) {
+    return res.status(422).json(failedMessage('Failed', 'No ext given'));
+  }
+  req.participent = judge;
+  return next();
+};
+
+Validator.prototype.validateCase = (req, res, next) => {
+  const _case = req.body.case || null;
+  if (!_case) {
+    return res.status(422).json(failedMessage('Failed', 'No case object'));
+  }
 };
 
 module.exports = Validator;
